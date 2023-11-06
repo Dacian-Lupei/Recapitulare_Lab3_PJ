eac13c99489265bd678eb7f01faa715fd66156bb
Lupei-Dacian <dacian.lupei@student.upt.ro>
2023-11-06 14:16:29 +0200

    Lab_3_Gata

--- src/P1/MainApp.java
+++ src/P1/MainApp.java
@@ -1,6 +1,5 @@
 package P1;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,7 +23,7 @@ public class MainApp {
                 }
             }
         }catch(IOException e){
-            e.printStackTrace();
+            System.err.println("Fisier gol!! " + e.getMessage());
         }
 
         for(Parabola p : parabole){
